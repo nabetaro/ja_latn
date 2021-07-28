11790e299abbb25d5b9fc00f5b689d01effc81fe
KURASAWA Nozomu <nabetaro@caldron.jp>
2021-07-28 20:20:04 +0900

    initial commit (bundle gem template)

---
+++ lib/ja_latn.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ja_latn/version"
+
+module JaLatn
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ja_latn/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JaLatn
+  VERSION = "0.1.0"
+end
